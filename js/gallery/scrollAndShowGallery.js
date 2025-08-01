eef7e8955bec38abc6176f26c112921b47baa4bc
damidDD5202 <damidDD5202@gmail.com>
2025-08-01 14:27:12 +0300

    feat: add js for section gallery for show

--- js/gallery/scrollAndShowGallery.js
+++ js/gallery/scrollAndShowGallery.js
@@ -1,5 +1,43 @@
 const gallery = document.querySelector('.gallery');
 
+// --- show ---
+
+
+const images = {
+    0: [
+        '../../assets/images/gallery11.svg',
+        '../../assets/images/gc2.svg',
+        '../../assets/images/gallery12.svg',
+        '../../assets/images/gc4.svg',
+    ],
+    1: [
+        '../../assets/images/tempgallery111.svg',
+        '../../assets/images/gallery22.svg',
+        '../../assets/images/gc3.svg',
+        '../../assets/images/gallery21.svg',
+    ]
+}
+
+const lineGallery = gallery.getElementsByTagName('div');
+
+function showGallery(){
+    console.log('sdfsdf')
+    for(let i = 0; i < 2; i++){
+        for(let j = 0; j < images[i].length; j++){
+            let image = document.createElement('img');
+            image.src = images[i][j];
+
+            lineGallery[i].appendChild(image);
+        }
+    }
+}
+
+showGallery();
+
+
+// --- scroll ---
+
+
 let isDown = false;
 let startX;
 let scrollLeft;
