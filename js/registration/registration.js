d3bbe6fa1c246cb44f3cde93f74ac570157971f6
damidDD5202 <damidDD5202@gmail.com>
2025-08-11 21:24:58 +0300

    feat: add rule for registration

--- js/registration/registration.js
+++ js/registration/registration.js
@@ -1,21 +1,126 @@
-import { getUsers, addUser } from "../../server/api.js";
+import { getUsers, addUser, getAllLogin } from "../../server/api.js";
+
+const commonPasswords = [
+    "Tes1_0000",
+    "password",
+    "123456",
+    "123456789",
+    "guest",
+    "QWERTY",
+    "12345678",
+    "111111",
+    "12345",
+    "col123456",
+    "123123",
+    "1234567",
+    "1234",
+    "1234567890",
+    "000000",
+    "555555",
+    "666666",
+    "123321",
+    "654321",
+    "7777777",
+    "123",
+    "d1lakiss",
+    "777777",
+    "110110jp",
+    "1111",
+    "987654321",
+    "121212",
+    "gizli",
+    "abc123",
+    "112233",
+    "azerty",
+    "159753",
+    "1q2w3e4r",
+    "54321",
+    "[email protected]",
+    "222222",
+    "qwertyuiop",
+    "qwerty123",
+    "qazwsx",
+    "vip",
+    "asdasd",
+    "123qwe",
+    "123654",
+    "iloveyou",
+    "a1b2c3",
+    "999999",
+    "Groupd2013",
+    "1q2w3e",
+    "usr",
+    "Liman1000",
+    "1111111",
+    "333333",
+    "123123123",
+    "9136668099",
+    "11111111",
+    "1qaz2wsx",
+    "password1",
+    "mar20lt",
+    "987654321",
+    "gfhjkm",
+    "159357",
+    "abcd1234",
+    "131313",
+    "789456",
+    "luzit2000",
+    "aaaaaa",
+    "zxcvbnm",
+    "asdfghjkl",
+    "1234qwer",
+    "88888888",
+    "dragon",
+    "987654",
+    "888888",
+    "qwe123",
+    "soccer",
+    "3601",
+    "asdfgh",
+    "master",
+    "samsung",
+    "12345678910",
+    "killer",
+    "1237895",
+    "1234561",
+    "12344321",
+    "daniel",
+    "00000",
+    "444444",
+    "101010",
+    "f–you",
+    "qazwsxedc",
+    "789456123",
+    "super123",
+    "qwer1234",
+    "123456789a",
+    "823477aA",
+    "147258369",
+    "unknown",
+    "98765",
+    "q1w2e3r4",
+    "232323"
+];
+
 
 const userData = {};
 
 let users;
+let login;
 
 document.addEventListener('DOMContentLoaded', async () => {
     await loadUsers();
-
+    
     inputHandler('name', (value) => (value.length < 2 || value.length > 20));
     inputHandler('surname', (value) => (value.length < 2 || value.length > 20));
     inputHandler('patronymic', (value) => value.length > 20);
-    inputHandler('telephone', (value) => false);
+    inputHandler('telephone', phoneValidation, phoneInputHandler);
     inputHandler('date', formattedDateOfBirthError, formattedDateOfBirthInput);
-    inputHandler('email', (value) => false);
-    inputHandler('login', (value) => false);
-    inputHandler('password', (value) => false);
-    inputHandler('repeatThePassword', (value) => false);
+    inputHandler('email', formattedEmailError);
+    loginInput();
+    passwordInput();
+    inputHandler('repeatThePassword', checkPasswordRepeat);
 });
   
 function inputHandler(someName, funcError, customInput = null){
@@ -41,17 +146,16 @@ function errorHandler(some, funcError){
 }
 
 function returnInputError(name) {
-    console.log(name) 
     const box = document.querySelector(`#${name}`);
 
     const input = box.querySelector('input');
-    const error = box.querySelector('p');
 
-    return { box, input, error };
+    return { box, input };
 }
 
 async function loadUsers() {
     users = await getUsers();
+    login = await getAllLogin();
 }
 
 async function maxIndex() {
@@ -62,6 +166,65 @@ async function maxIndex() {
     return maxId;
 }
 
+
+
+// ----------- TELEPHONE ----------------
+
+
+function phoneInputHandler(inputElement) {
+    const cursorPosition = inputElement.selectionStart;
+    let value = inputElement.value.replace(/[^0-9]/g, ''); // Удаляем всё, кроме цифр
+    let formattedValue = '+375 (';
+
+    if(value.length == 1){
+        value = '375' + value;
+    }
+    if (value.length > 3) {
+        formattedValue += value.substring(3, 5);
+    }
+    if (value.length > 5) {
+        formattedValue += ') ' + value.substring(5, 8);
+    }
+    if (value.length > 8) {
+        formattedValue += '-' + value.substring(8, 10);
+    }
+    if (value.length > 10) {
+        formattedValue += '-' + value.substring(10, 12);
+    }
+
+    inputElement.value = formattedValue;
+
+    let newCursorPosition = cursorPosition;
+    
+    newCursorPosition = Math.max(7, formattedValue.length);
+
+    inputElement.setSelectionRange(newCursorPosition, newCursorPosition);
+    
+    userData.telephone = formattedValue;
+}
+
+
+function phoneValidation(value) {
+    const phoneRegex = /^\+375 \(\d{2}\) \d{3}-\d{2}-\d{2}$/;
+    return !phoneRegex.test(value);
+}
+
+
+// ----------- EMAIL ----------------
+
+
+
+function formattedEmailError(value) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    const [localPart] = value.split('@'); // Получаем часть до @
+
+    if (localPart.length < 2 || localPart.length > 40) {
+        return true; 
+    }
+
+    return !regex.test(value);
+}
 
 
 // ----------- DATE OF BIRTH ----------------
@@ -128,6 +291,159 @@ function getMaxDaysInMonth(month, year) {
 }
 
 
+// ----------- LOGIN ----------------
+
+const loginBox = document.querySelector(`#login`);
+const inputLogin = loginBox.querySelector('input');
+const loginBoxButton = loginBox.querySelector(`#loginGenerate`);
+
+let countChance = 0;
+
+loginBoxButton.addEventListener('click', async () => {
+    if(countChance < 5){
+        const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        let nickname = '';
+        const length = 8; 
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            nickname += characters[randomIndex];
+        }
+
+        countChance++;
+        
+        inputLogin.value = nickname
+    }else{
+        loginBox.classList.add('generate');
+    }
+});
+
+function loginInput(){
+    inputLogin.addEventListener('input', () =>{
+        userData['login'] = inputLogin.value.trim();
+        loginBox.classList.remove('error');   
+        loginBox.classList.remove('generate');   
+    })
+
+    inputLogin.addEventListener('blur', () => {
+        if(login.includes(inputLogin.value) || inputLogin.value.length < 2 || inputLogin.value.length > 30) loginBox.classList.add('error');
+    });
+}
+
+
+// ----------- PASSWORD ----------------
+
+const passwordBox = document.querySelector(`#password`);
+const inputPassword = passwordBox.querySelector('input');
+const passwordBoxButton = passwordBox.querySelector(`#passwordGenerate`);
+
+const passwordBoxRepeat = document.querySelector(`#repeatThePassword`);
+const inputPasswordBoxRepeat = passwordBoxRepeat.querySelector('input');
+
+inputPasswordBoxRepeat.addEventListener('paste', (event) => {
+    event.preventDefault(); 
+});
+
+let countChancePassword = 0;
+
+passwordBoxButton.addEventListener('click', async () => {
+    passwordBox.className = 'form';
+
+    if (countChancePassword < 5) {
+        const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+        const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const digits = '0123456789';
+        const specialCharacters = '!@#$%^&*(),.?":{}|<>+=-_';
+        
+        // Убедимся, что пароль содержит хотя бы один символ из каждой категории
+        let password = '';
+        password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length)); // 1 строчная
+        password += upperCase.charAt(Math.floor(Math.random() * upperCase.length)); // 1 заглавная
+        password += digits.charAt(Math.floor(Math.random() * digits.length)); // 1 цифра
+        password += specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length)); // 1 спецсимвол
+
+        const allCharacters = lowerCase + upperCase + digits + specialCharacters;
+
+        // Генерируем оставшиеся символы пароля
+        const length = 14; 
+        for (let i = password.length; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * allCharacters.length);
+            password += allCharacters[randomIndex];
+        }
+
+        // Перемешиваем пароль для случайного порядка символов
+        password = password.split('').sort(() => Math.random() - 0.5).join('');
+
+        countChancePassword++;
+        
+        inputPassword.value = password;
+    } else {
+        passwordBox.classList.add('generate');
+    }
+});
+
+function passwordInput(){
+    inputPassword.addEventListener('input', () =>{
+        userData['password'] = inputPassword.value.trim();
+        passwordBox.className = 'form';
+    })
+
+    inputPassword.addEventListener('blur', () => {
+        const password = inputPassword.value;
+        inputPasswordBoxRepeat.focus();
+
+        if(!/[a-z]/.test(password)){
+            addErrorPassword('one');
+        }else{
+            if(!/[A-Z]/.test(password)){
+                addErrorPassword('two');
+            }else{
+                if(!/\d/.test(password)){
+                    addErrorPassword('three');
+                }else{
+                    if (!/[!@#$%^&*(),.?":{}|<>_\-+=]/.test(password)) {
+                        addErrorPassword('four');
+                    }else{
+                        if(password.length < 8 || password.length > 20){
+                            addErrorPassword('five');
+                        }else{
+                            if(commonPasswords.includes(password)){
+                                addErrorPassword('six');
+                            }
+                        }
+                    }
+                }
+            }
+        }       
+        
+        regButton.focus();
+    });
+}
+
+function addErrorPassword(number){
+    passwordBox.classList.add('error');
+    passwordBox.classList.add(number);
+}
+
+
+// ----------- REPEAT PASSWORD ----------------
+
+
+function checkPasswordRepeat(value) {
+    return !(inputPassword.value === value);
+}
+
+
+//---------show/hide----------
+
+const showHide = document.getElementById('showHide');
+showHide.addEventListener('click', () => {
+    const isText = passwordBoxButton.type === 'text';
+
+    inputPassword.type = isText ? 'password' : 'text';
+    inputPasswordBoxRepeat.type = isText ? 'password' : 'text';
+})
+
 
 // ------------------ REGISTRATION ---------------------
 
@@ -160,7 +476,11 @@ function checkField() {
     const hasError = document.querySelector('.error') !== null;
 
     const checkbox = document.querySelector('#agree input[type="checkbox"]');
-    const isCheckboxChecked = checkbox ? checkbox.checked : false;
+    const isCheckboxChecked = checkbox ? checkbox.checked : false; 
+
+    if (hasError || !isCheckboxChecked) {
+        alert('Please fix the errors OR the user agreement is checked!');
+    }
 
     return !hasError && isCheckboxChecked;
 }
--- server/api.js
+++ server/api.js
@@ -65,6 +65,23 @@ export const deleteUser = async (userId) => {
 };
 
 
+export const getAllLogin = async (user) => {
+    const response = await fetch('http://localhost:3000/users', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (!response.ok) {
+        throw new Error('Ошибка при получении пользователей');
+    }
+
+    const users = await response.json();
+
+    const logins = users.map(user => user.login);
+    
+    return logins;
+};
 
 //--- cart ----
 
